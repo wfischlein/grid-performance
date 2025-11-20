7c15b2c0b6e006eaa82614a13ab13f6698640ac1
Wolfgang Fischlein <wolfgang@vaadin.com>
2025-11-20 10:32:44 +0000

    formatted, updated README

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,35 +1,72 @@
 package com.example.application.views.helloworld;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
-@PageTitle("Hello World")
-@Route("")
-@Menu(order = 0, icon = LineAwesomeIconUrl.GLOBE_SOLID)
-public class HelloWorldView extends HorizontalLayout {
+import java.util.UUID;
+import java.util.stream.Stream;
 
-    private TextField name;
-    private Button sayHello;
+@PageTitle("Hello World") @Route("") @Menu(order = 0, icon = LineAwesomeIconUrl.GLOBE_SOLID) public class HelloWorldView
+		extends VerticalLayout {
+	/**
+	 * Enter like 500 to the 'column count' field then press 'Generate'
+	 * Then enter like 501 to the 'column count' field and press 'Generate' again
+	 */
+	public HelloWorldView() {
+		setSizeFull();
+		final Grid<DataElement> grid = new Grid<>();
+		grid.setItems(new ListDataProvider<>(DataElement.generate(50).toList()));
+		grid.setSizeFull();
+		grid.addComponentColumn(dataElement -> new Span(dataElement.value)).setFrozen(true);
+		final IntegerField countField = new IntegerField("column count");
+		final Button generateColumns = new Button("Generate");
 
-    public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+		generateColumns.addClickListener(e -> {
+			grid.removeAllColumns();
+			grid.addComponentColumn(dataElement -> new Span(dataElement.value)).setFrozen(true);
+			int count = (countField.getValue() > 0 ? countField.getValue() : 1);
+			ColumnSpec.generate(count).forEach(
+					columnSpec -> grid.addComponentColumn(pDataElement -> new Span(columnSpec.getPropertyName()))
+							.setHeader(columnSpec.getPropertyName()));
+		});
+		setMargin(true);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+		HorizontalLayout panel = new HorizontalLayout(countField, generateColumns);
+		panel.setVerticalComponentAlignment(Alignment.BASELINE, countField, generateColumns);
+		add(panel, grid);
+	}
 
-        add(name, sayHello);
-    }
+	public static final class ColumnSpec {
+		static Stream<ColumnSpec> generate(int count) {
+			return Stream.generate(ColumnSpec::new).limit(count);
+		}
 
+		private String value = UUID.randomUUID().toString();
+
+		public String getPropertyName() {
+			return value;
+		}
+	}
+
+
+	public static final class DataElement {
+		private String value = UUID.randomUUID().toString();
+
+		static Stream<DataElement> generate(int count) {
+			return Stream.generate(DataElement::new).limit(count);
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
 }
