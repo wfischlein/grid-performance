42ba74f546d9502d79c0a6991502d2f0478fb78e
Wolfgang Fischlein <wolfgang@vaadin.com>
2025-12-16 12:37:49 +0000

    Added 're-create grid'-option to compare with removeAllColumns

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,6 +1,7 @@
 package com.example.application.views.helloworld;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -17,32 +18,53 @@ import java.util.stream.Stream;
 
 @PageTitle("Hello World") @Route("") @Menu(order = 0, icon = LineAwesomeIconUrl.GLOBE_SOLID) public class HelloWorldView
 		extends VerticalLayout {
-	/**
+
+	private Grid<DataElement> grid;
+/**
 	 * Enter like 500 to the 'column count' field then press 'Generate'
 	 * Then enter like 501 to the 'column count' field and press 'Generate' again
 	 */
 	public HelloWorldView() {
 		setSizeFull();
+		grid = createGrid();
+		final IntegerField countField = new IntegerField("column count");
+		countField.setValue(500);
+		updateGrid(grid, 500);
+
+		Checkbox checkbox = new Checkbox("Re-create grid");
+
+		final Button generateColumns = new Button("Generate Columns");
+
+		generateColumns.addClickListener(e -> {
+			if (checkbox.getValue()) {
+				remove(grid);
+				grid = createGrid();
+				add(grid);
+			}
+			updateGrid(grid, countField.getValue());
+		});
+		setMargin(true);
+
+		HorizontalLayout panel = new HorizontalLayout(countField, generateColumns, checkbox);
+		panel.setVerticalComponentAlignment(Alignment.BASELINE, countField, generateColumns, checkbox);
+		add(panel, grid);
+	}
+
+	private Grid<DataElement> createGrid() {
 		final Grid<DataElement> grid = new Grid<>();
 		grid.setItems(new ListDataProvider<>(DataElement.generate(50).toList()));
 		grid.setSizeFull();
 		grid.addComponentColumn(dataElement -> new Span(dataElement.value)).setFrozen(true);
-		final IntegerField countField = new IntegerField("column count");
-		final Button generateColumns = new Button("Generate");
+		return grid;
+	}
 
-		generateColumns.addClickListener(e -> {
-			grid.removeAllColumns();
-			grid.addComponentColumn(dataElement -> new Span(dataElement.value)).setFrozen(true);
-			int count = (countField.getValue() > 0 ? countField.getValue() : 1);
-			ColumnSpec.generate(count).forEach(
-					columnSpec -> grid.addComponentColumn(pDataElement -> new Span(columnSpec.getPropertyName()))
-							.setHeader(columnSpec.getPropertyName()));
-		});
-		setMargin(true);
-
-		HorizontalLayout panel = new HorizontalLayout(countField, generateColumns);
-		panel.setVerticalComponentAlignment(Alignment.BASELINE, countField, generateColumns);
-		add(panel, grid);
+	private void updateGrid(Grid<DataElement> grid, Integer columnCount) {
+		grid.removeAllColumns();
+		grid.addComponentColumn(dataElement -> new Span(dataElement.value)).setFrozen(true);
+		int count = (columnCount > 0 ? columnCount : 1);
+		ColumnSpec.generate(count).forEach(
+				columnSpec -> grid.addComponentColumn(pDataElement -> new Span(columnSpec.getPropertyName()))
+						.setHeader(columnSpec.getPropertyName()));
 	}
 
 	public static final class ColumnSpec {
